4720f6242e40617f39cb7208343b0164c2325c10
malasiot <malasiot@iti.gr>
2018-11-05 10:28:37 +0200

    fix

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -12,6 +12,8 @@ string readFileToString(const string &fileName)
 {
     ifstream strm(fileName) ;
 
+    if ( !strm ) return string() ;
+
     strm.seekg(0, ios::end);
     size_t length = strm.tellg();
     strm.seekg(0,std::ios::beg);
@@ -24,7 +26,7 @@ string readFileToString(const string &fileName)
 
 bool fileExists(const string &p) {
     struct stat buffer;
-    return ::stat(p.c_str(), &buffer) == 0 ;
+    return ::stat(p.c_str(), &buffer) == 0 &&  S_ISREG(buffer.st_mode);
 }
 
 time_t fileLastWriteTime(const std::string &p) {
